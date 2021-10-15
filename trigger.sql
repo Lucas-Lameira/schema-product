9c7f12fa5b18c6412efd0b275fb17383036b5fcf
Lucas-Lameira <lucasslameira@gmail.com>
2021-10-14 23:17:05 -0300

    project files

--- trigger.sql
+++
@@ -1,9 +0,0 @@
--- disparado quando um com comando dml ocorrer em uma tabela  (INSERT, UPDATE, DELETE)
-
--- verificar se dado integro
--- validar dados (tipos de dados, faiza de valor)
--- rastreamento de registros de logs na tabela
--- é armazenado em um arquivo separado
--- arquivar registros excluidos
--- é disparado quando ocorre um evento dml
-
